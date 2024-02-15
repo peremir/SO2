ff7166846ebadf3f47db93dfe0f82f83f61ba8d5
Pere Mir <peremirolivares@gmail.com>
2024-02-15 17:26:10 +0100

    suma.s funcionant :)

--- zeos/user.c
+++ zeos/user.c
@@ -4,12 +4,16 @@ char buff[24];
 
 int pid;
 
+//definir funcio 
+int addAsm(int par1, int par2);
+
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    
+  int x = addAsm(0x42, 0x666);  
+  
   while(1) { }
 }
